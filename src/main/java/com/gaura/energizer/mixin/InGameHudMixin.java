3662ce20de054fbef240640dc0079e36138de339
Gaura <gauraaaaaaaa@gmail.com>
2024-06-03 15:08:14 +0200

    Add animation to vigor effect + Change license to MIT

--- src/main/java/com/gaura/energizer/mixin/InGameHudMixin.java
+++ src/main/java/com/gaura/energizer/mixin/InGameHudMixin.java
@@ -6,7 +6,9 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -17,7 +19,7 @@ public class InGameHudMixin {
     private static final Identifier GUI_ICONS = new Identifier("textures/gui/icons.png");
     private static final Identifier STAMINA_ICONS = new Identifier(Energizer.MOD_ID, "textures/stamina/stamina_icons.png");
 
-    private long lastFullFillTime = -1;
+    private int lastFullFillTime = -1;
 
     @Redirect(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"))
     private void renderStaminaBarAndAdjustAirBar(DrawContext context, Identifier texture, int x, int y, int u, int v, int width, int height) {
@@ -35,61 +37,90 @@ public class InGameHudMixin {
 
         if (texture.equals(GUI_ICONS) && v == 27) {
 
-            x = (client.getWindow().getScaledWidth() / 2) + Energizer.CONFIG.x_offset;
-            y = client.getWindow().getScaledHeight() + Energizer.CONFIG.y_offset;
-
-            float maxStamina = (float) client.player.getAttributeValue(Energizer.STAMINA_ATTRIBUTE);
-            float currentStamina = client.player.getDataTracker().get(Energizer.STAMINA_DATA);
-
-            boolean hasHunger = client.player.hasStatusEffect(StatusEffects.HUNGER);
-            boolean hasVigor = client.player.hasStatusEffect(Energizer.VIGOR);
-            boolean stopSprint = ((IPlayerEntity) client.player).getStopSprint();
-
-            int lines = (int) Math.ceil(maxStamina / 20);
-            int fullIconsPerLine = (int) (currentStamina / 2);
-            boolean halfIcon = Math.floor(currentStamina) % 2 != 0;
-            int backgroundsPerLine = (int) Math.ceil(maxStamina / 2);
-
-            if (currentStamina == maxStamina) {
-
-                if (lastFullFillTime == -1) {
-
-                    lastFullFillTime = client.world.getTime();
-                }
-            }
-            else {
-
-                lastFullFillTime = -1;
-            }
-
-            int yDecrement = getYDecrement(maxStamina);
-
-            for (int line = 0; line < lines; line++, fullIconsPerLine -= 10, backgroundsPerLine -= 10) {
-
-                for (int i = 0; i < Math.min(backgroundsPerLine, 10); i++) {
-
-                    u = ((lastFullFillTime != -1) && ((client.world.getTime() - lastFullFillTime) < 3) && Energizer.CONFIG.stamina_blink) ? 9 : 0;
-                    context.drawTexture(STAMINA_ICONS, x - i * 8, y - line * yDecrement, u, 0, 9, 9, 81, 9);
-                }
-
-                for (int i = 0; i < Math.min(fullIconsPerLine, 10); i++) {
-
-                    u = hasVigor ? 72 : stopSprint ? 36 : hasHunger ? 54 : 18;
-                    context.drawTexture(STAMINA_ICONS, x - i * 8, y - line * yDecrement, u, 0, 9, 9, 81, 9);
-                }
-
-                if (halfIcon && fullIconsPerLine < 10) {
-
-                    u = stopSprint ? 45 : hasHunger ? 63 : 27;
-                    context.drawTexture(STAMINA_ICONS, x - fullIconsPerLine * 8, y - line * yDecrement, u, 0, 9, 9, 81, 9);
-                    halfIcon = false;
-                }
-            }
+            renderStaminaBar(context, client);
         }
         else {
 
             context.drawTexture(texture, x, y, u, v, width, height);
         }
+    }
+
+    private void renderStaminaBar(DrawContext context, MinecraftClient client) {
+
+        int x = (client.getWindow().getScaledWidth() / 2) + Energizer.CONFIG.x_offset;
+        int y = client.getWindow().getScaledHeight() + Energizer.CONFIG.y_offset;
+        int vigorIndex = -1;
+
+        float maxStamina = (float) client.player.getAttributeValue(Energizer.STAMINA_ATTRIBUTE);
+        float currentStamina = client.player.getDataTracker().get(Energizer.STAMINA_DATA);
+
+        boolean hasHunger = client.player.hasStatusEffect(StatusEffects.HUNGER);
+        boolean hasVigor = client.player.hasStatusEffect(Energizer.VIGOR);
+        boolean stopSprint = ((IPlayerEntity) client.player).getStopSprint();
+
+        int lines = (int) Math.ceil(maxStamina / 20);
+        int fullIconsPerLine = (int) (currentStamina / 2);
+        boolean halfIcon = Math.floor(currentStamina) % 2 != 0;
+        int backgroundsPerLine = (int) Math.ceil(maxStamina / 2);
+
+        if (hasVigor) {
+
+            vigorIndex = client.inGameHud.getTicks() % MathHelper.ceil(maxStamina + 5.0F);
+        }
+
+        if (currentStamina == maxStamina) {
+
+            if (lastFullFillTime == -1) {
+
+                lastFullFillTime = client.inGameHud.getTicks();
+            }
+        }
+        else {
+
+            lastFullFillTime = -1;
+        }
+
+        int yDecrement = getYDecrement(maxStamina);
+
+        for (int line = 0; line < lines; line++, fullIconsPerLine -= 10, backgroundsPerLine -= 10) {
+
+            for (int i = 0; i < Math.min(backgroundsPerLine, 10); i++) {
+
+                int u = ((lastFullFillTime != -1) && ((client.inGameHud.getTicks() - lastFullFillTime) < 3) && Energizer.CONFIG.stamina_blink) ? 9 : 0;
+
+                int vigor = getVigorIndex(i, maxStamina, vigorIndex, line);
+
+                context.drawTexture(STAMINA_ICONS, x - i * 8, y - line * yDecrement - vigor, u, 0, 9, 9, 81, 9);
+            }
+
+            for (int i = 0; i < Math.min(fullIconsPerLine, 10); i++) {
+
+                int u = hasVigor ? 72 : stopSprint ? 36 : hasHunger ? 54 : 18;
+
+                int vigor = getVigorIndex(i, maxStamina, vigorIndex, line);
+
+                context.drawTexture(STAMINA_ICONS, x - i * 8, y - line * yDecrement - vigor, u, 0, 9, 9, 81, 9);
+            }
+
+            if (halfIcon && fullIconsPerLine < 10) {
+
+                int u = stopSprint ? 45 : hasHunger ? 63 : 27;
+                context.drawTexture(STAMINA_ICONS, x - fullIconsPerLine * 8, y - line * yDecrement, u, 0, 9, 9, 81, 9);
+                halfIcon = false;
+            }
+        }
+    }
+
+    private int getVigorIndex(int i, float maxStamina, int vigorIndex, int line) {
+
+        int index = 0;
+
+        if ((i < maxStamina) && (i == (vigorIndex - (line * 10)))) {
+
+            index = 2;
+        }
+
+        return index;
     }
 
     private int getYDecrement(float maxStamina) {
