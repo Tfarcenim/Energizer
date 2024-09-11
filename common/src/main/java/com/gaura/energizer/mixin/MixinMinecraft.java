722ca6e6171cbd72c45169b5ebe066d114a3ff05
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-09-11 11:50:02 -0400

    move into multiloader template

---
+++ common/src/main/java/com/gaura/energizer/mixin/MixinMinecraft.java
@@ -0,0 +1,19 @@
+package com.gaura.energizer.mixin;
+
+import com.gaura.energizer.CommonClass;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+    
+    @Inject(at = @At("TAIL"), method = "<init>")
+    private void init(CallbackInfo info) {
+        
+        CommonClass.LOG.info("This line is printed by an example mod common mixin!");
+        CommonClass.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
+    }
+}
